9c2bfad3883229f6e8f76017c5c75dd1cabf459d
Paulo Rodrigues <pauloadesr@hotmail.com>
2021-06-06 11:59:07 -0300

    create PokemonCell View

---
+++ pokedex-ios-swift/View/PokemonCell.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonCell.swift
+//  pokedex-ios-swift
+//
+//  Created by Paulo Rodrigues on 06/06/21.
+//
+
+import SwiftUI
+
+struct PokemonCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonCell()
+    }
+}
