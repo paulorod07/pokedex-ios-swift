a43d799fa8311d7f92cd8be3fd410cd45460bcc5
Paulo Rodrigues <pauloadesr@hotmail.com>
2021-06-06 11:45:56 -0300

    initial structure

---
+++ pokedex-ios-swift/pokedex_ios_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_ios_swiftApp.swift
+//  pokedex-ios-swift
+//
+//  Created by Paulo Rodrigues on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_ios_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
