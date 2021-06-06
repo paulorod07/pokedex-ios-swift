070c553481b319017be0cafa1b87fa7ba50d8d19
Paulo Rodrigues <pauloadesr@hotmail.com>
2021-06-06 12:45:41 -0300

    updating PokemonCell View

--- pokedex-ios-swift/View/PokemonCell.swift
+++ pokedex-ios-swift/View/PokemonCell.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct PokemonCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(alignment: .leading) {
+                Text("Bulbasaur")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.top, 8)
+                    .padding(.leading)
+                
+                HStack {
+                    Text("poison")
+                        .font(.subheadline).bold()
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20).fill(Color.white.opacity(0.25))
+                        )
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 24)
+
+                    Image("Image")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 68, height: 68)
+                        .padding([.bottom, .trailing], 4)
+                }
+            }
+        }
+        .background(Color.green)
+        .cornerRadius(12)
+        .shadow(color: .green, radius: 6, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+    
     }
 }
 
