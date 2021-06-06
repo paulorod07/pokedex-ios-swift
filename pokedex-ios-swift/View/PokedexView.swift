d49403e1c4d11e2e28bcdb5c08d6b50975c50ebc
Paulo Rodrigues <pauloadesr@hotmail.com>
2021-06-06 13:08:39 -0300

    Creating and updating PokedexView

---
+++ pokedex-ios-swift/View/PokedexView.swift
@@ -0,0 +1,31 @@
+//
+//  PokedexView.swift
+//  pokedex-ios-swift
+//
+//  Created by Paulo Rodrigues on 06/06/21.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing: 16) {
+                    ForEach(0..<151) { _ in
+                        PokemonCell()
+                    }
+                }
+            }
+            .navigationTitle("Pokedex")
+        }
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
